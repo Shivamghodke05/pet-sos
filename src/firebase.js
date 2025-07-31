307424c181b137928336b5e4e3f91e58020ebb77
Shivam Ghodke <shivamghodke05@gmail.com>
2025-07-31 23:01:03 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,19 +2,16 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//fill this info with your firebase config data that u will create
 const firebaseConfig = {
-  apiKey: "AIzaSyAp5ihWxHzyWDCZLd1yYGO9NIC_2RT9pq4",
-  authDomain: "pet-sos-web.firebaseapp.com",
-  projectId: "pet-sos-web",
-  storageBucket: "pet-sos-web.firebasestorage.app",
-  messagingSenderId: "468811225879",
-  appId: "1:468811225879:web:c3f892e9983926b3e4b552",
-  measurementId: "G-THQC3L5T4Z",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
